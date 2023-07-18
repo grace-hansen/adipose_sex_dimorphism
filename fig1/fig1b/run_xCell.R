f389f646471fc35ae72fcd44dc3b9f9b0f93329b
Grace Hansen <42852986+grace-hansen@users.noreply.github.com>
2023-07-18 14:59:14 -0500

    Add files via upload

--- fig1/fig1b/run_xCell.R
+++ fig1/fig1b/run_xCell.R
@@ -1,1 +1,25 @@
-/home/grace/midway/xCell/scripts/run_xCell.R
+#!/usr/bin/Rscript
+args=commandArgs(trailingOnly=TRUE)
+library(tidyverse)
+library(xCell)
+library(data.table)
+
+
+dat<-fread(args[1],data.table=FALSE)
+rownames(dat)<-make.names(dat[,1],unique=TRUE)
+dat<-dat[,-1]
+
+working_dir<-dirname(args[1])
+prefix<-strsplit(basename(args[1]),'[.]')[[1]][1]
+
+print(working_dir)
+print(prefix)
+if (len(args)>1) {
+  celltypes<-strsplit(args[2],',')[[1]]
+} else {
+  celltypes<-c("Adipocytes","Epithelial cells","Hepatocytes","Keratinocytes","Myocytes","Neurons","Neutrophils")
+}
+
+out<-xCellAnalysis(dat)
+out<-out[which(rownames(out)%in%celltypes),]
+write.table(out,paste(working_dir,"/",prefix,"_results_7celltypes.txt",sep=''),quote=FALSE,sep='\t')
